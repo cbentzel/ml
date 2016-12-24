67c3b5fb2985351e7224c3909f0226fea8519321
Chris Bentzel <cbentzel@chromium.org>
2016-12-24 17:10:46 -0500

    Gradient descent written

--- w2/gradientDescent.m
+++ w2/gradientDescent.m
@@ -1,4 +1,4 @@
-function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+function [theta, J_history] = gradientDescent(X, y, theta_arg, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
@@ -6,6 +6,8 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
+step = alpha / m;
+theta = theta_arg
 
 for iter = 1:num_iters
 
@@ -17,17 +19,17 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
-
+    diffs = X*theta - y;
+    grad_1 = sum(diffs.*X(:, 1));
+    grad_2 = sum(diffs.*X(:, 2));
+    grad = [grad_1; grad_2];
+    theta = theta - step*grad;
+    
     % ============================================================
 
     % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
-
+    cur_cost = computeCost(X, y, theta);
+    J_history(iter) = cur_cost;
 end
 
 end
