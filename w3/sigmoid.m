dfaaf23d70a544a5e26ed1f6275d172e27dccf22
Chris Bentzel <cbentzel@chromium.org>
2016-12-27 20:02:24 -0500

    Simoid function

--- w3/sigmoid.m
+++ w3/sigmoid.m
@@ -9,8 +9,8 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-
-
+denom = 1.0 + e.^-z;
+g = 1.0 ./ denom;
 
 
 % =============================================================
